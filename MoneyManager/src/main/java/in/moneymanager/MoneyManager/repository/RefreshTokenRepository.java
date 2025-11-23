ec63d1c1d6ce6111fee27028ab4ac3b4006316b6
dhruvjain01 <dhruvjain2001@gmail.com>
2025-11-23 19:16:43 +0530

    Rotate Token Implementation.

--- MoneyManager/src/main/java/in/moneymanager/MoneyManager/repository/RefreshTokenRepository.java
+++ MoneyManager/src/main/java/in/moneymanager/MoneyManager/repository/RefreshTokenRepository.java
@@ -1,12 +1,7 @@
 package in.moneymanager.MoneyManager.repository;
 
 import in.moneymanager.MoneyManager.entity.RefreshToken;
-import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Lock;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
--- MoneyManager/src/main/java/in/moneymanager/MoneyManager/service/ProfileService.java
+++ MoneyManager/src/main/java/in/moneymanager/MoneyManager/service/ProfileService.java
@@ -8,17 +8,14 @@ import in.moneymanager.MoneyManager.repository.ProfileRepository;
 import in.moneymanager.MoneyManager.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.HashMap;
 import java.util.Map;
