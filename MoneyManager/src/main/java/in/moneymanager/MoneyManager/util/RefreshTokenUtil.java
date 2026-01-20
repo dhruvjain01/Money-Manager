3988ec94a554b1408064dcaf034c8de4794ee375
Dhruv Jain <109064569+dhruvjain01@users.noreply.github.com>
2026-01-20 19:44:17 +0530

    Improved Authentication flow (#5)
    Improved Authentication flow merge

---
+++ MoneyManager/src/main/java/in/moneymanager/MoneyManager/util/RefreshTokenUtil.java
@@ -0,0 +1,23 @@
+package in.moneymanager.MoneyManager.util;
+
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.UUID;
+
+@Component
+public class RefreshTokenUtil {
+
+    public String generateToken() {
+        return UUID.randomUUID().toString().replace("-", "");
+    }
+
+    public Instant expiry(int days) {
+        return Instant.now().plus(days, ChronoUnit.DAYS);
+    }
+
+    public Instant expiryM(int minutes) {
+        return Instant.now().plus(minutes, ChronoUnit.MINUTES);
+    }
+}
